b10c01aa6377147839b5efd07b1f23893de32691
seungsang <rovin000@hansung.ac.kr>
2024-06-06 00:26:20 +0900

    스프링 시큐리티 연습

--- src/test/java/kr/ac/hansung/cse/hellospringbootsecurity/HelloSpringBootSecurityApplicationTests.java
+++ src/test/java/kr/ac/hansung/cse/hellospringbootsecurity/HelloSpringBootSecurityApplicationTests.java
@@ -1,13 +1,20 @@
 package kr.ac.hansung.cse.hellospringbootsecurity;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
-class HelloSpringBootSecurityApplicationTests {
+class HellospringsecurityApplicationTests {
+
+    @Autowired
+    private PasswordEncoder encoder;
 
     @Test
-    void contextLoads() {
+    void generateHashedPassword() {
+        String pwd = encoder.encode("alicepw");
+        System.out.println(pwd);
     }
 
 }
